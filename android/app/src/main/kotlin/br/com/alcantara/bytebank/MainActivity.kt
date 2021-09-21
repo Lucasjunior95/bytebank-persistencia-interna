7f7fbc5b17b0745a7052d35c85b7d905ec59c832
Lucas <lucasjunior810@gmail.com>
2021-09-21 20:38:14 -0300

    Projeto inicial bytebank

---
+++ android/app/src/main/kotlin/br/com/alcantara/bytebank/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.alcantara.bytebank
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(ByteBankApp());
+
+class ByteBankApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dashboard'),
+        ),
+      ),
+    );
+  }
+}
+
